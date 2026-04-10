5610c0ca03fe552606473caef83bcd5521a430fb
Joelhawkins1 <joelhawkins2552@outlook.com>
2026-04-09 22:35:45 -0500

    commit -m

--- QrCodeSc/main.cpp
+++ QrCodeSc/main.cpp
@@ -1,47 +1,73 @@
+#include <opencv2/opencv.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/utils/logger.hpp>
+#include <windows.h>
+#include <shellapi.h>
+#include <string>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
-void display(Mat &im,Mat &bbox)
+
+int main()
 {
-	int n = bbox.rows;
-	for (int i; i < n; i++)
+	Mat frame; // creats the variable frame using the "Mat" class holds a picture
+	VideoCapture cap; // Initalizing VideoCapture Class creating a object named cap.
+	QRCodeDetector decode; // Initalizing 
+
+	cap.open(0, cv::CAP_DSHOW); // Openining the systems defualt camera 
+	cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+	cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+
+	if (!cap.isOpened()) // Checks to see if camera is opened if not gives a error message
 	{
-		line(im, Point2i(bbox.at<float>(i, 0), bbox.at<float>(i, 1)), Point2i(bbox.at<float>((i + 1) % n, 0), bbox.at<float>((i + 1) % n, 1)), Scalar(255, 0, 0), 3);
+		cout << "ERROR! Unable to open camera\n";
+		return -1;
 	}
-	imshow("Result", im);
+
+	for(;;) // creating a infinte loop
+	{
+		cap.read(frame); // calls the videocapture object reads it and stores it in the frame variable created using the mat class.
+
+		if (frame.empty()) // Testing to see if the image pulled is there.
+		{
+			cout << "ERROR! Blank image";
+			break;
+		}
+
+		while (true) {
+			cap >> frame;
+			if (frame.empty()) break;
+
+			Mat points, straight_qrcode;
+			string data = decode.detectAndDecode(frame, points, straight_qrcode);
+				if (!data.empty()) {
+					cout << "QR Code Detected: " << data << endl;
+					ShellExecuteA(NULL, "open", data.c_str(), NULL, NULL, SW_SHOWNORMAL); // Uses the windows shell to open the url with your systems browser.
+
+					for (int i = 0; i < 4; i++) {
+						cv::line(frame, points.at<cv::Point2f>(i),
+							points.at<cv::Point2f>((i + 1) % 4),
+							cv::Scalar(0, 255, 0), 3);
+					}
+				}
+
+			cv::imshow("Camera Feed", frame);
+			if (cv::waitKey(1) == 27) break; // Press 'Esc' to exit
+		}
+	}
+	return 0;
 }
 
+/*
+	Refrences.
 
-int main(int argc, char* argv[])
-{
-	// Read image
-	Mat inputImage;
-	if (argc > 1)
-		inputImage = imread(argv[1]);
-	else
-		inputImage = imread("qrcode-learnopencv.jpg");
+	Windows.h https://learn.microsoft.com/en-us/windows/win32/shell/launch
+	VideoCapture Documentation https://docs.opencv.org/4.x/d8/dfe/classcv_1_1VideoCapture.html#a57c0e81e83e60f36c83027dc2a188e80
+	QRCodeDetector Documnetation https://docs.opencv.org/4.x/de/dc3/classcv_1_1QRCodeDetector.html#a397753e5eda6c54f10671e9013682bd7
 
-	QRCodeDetector qrDecoder = QRCodeDetector::QRCodeDetector();
-
-	Mat bbox, rectifiedImage;
-
-	std::string data = qrDecoder.detectAndDecode(inputImage, bbox, rectifiedImage);
-	if (data.length() > 0)
-	{
-		cout << "Decoded Data : " << data << endl;
-
-		display(inputImage, bbox);
-		rectifiedImage.convertTo(rectifiedImage, CV_8UC3);
-		imshow("Rectified QRCode", rectifiedImage);
-
-		waitKey(0);
-	}
-	else
-		cout << "QR Code not detected" << endl;
-}
+*/
